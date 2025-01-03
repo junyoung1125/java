e4cff9844e0f555b5ffb87afcc774f730a080356
wnsdud060451 <wnsdud060451@gmail.com>
2025-01-03 16:24:51 +0900

    add ThisJava

---
+++ ThisJava/src/ch04/sec05/SumFrom1To100Example.java
@@ -0,0 +1,19 @@
+package ch04.sec05;
+
+//p132 실습하기
+public class SumFrom1To100Example {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+		int i = 1;
+		while (i <= 100) {
+			sum += i;
+			i++;
+
+			System.out.println("1~" + (1 - i) + "합 :" + sum);
+
+		}
+
+	}
+}
