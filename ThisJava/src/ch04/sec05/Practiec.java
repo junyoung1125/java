ff381956a9fb66a8568fb373ffea31e7d68359c2
wnsdud060451 <wnsdud060451@gmail.com>
2025-01-03 17:35:19 +0900

    add ThisJava

--- ThisJava/src/ch04/sec05/Practiec.java
+++ ThisJava/src/ch04/sec05/Practiec.java
@@ -4,13 +4,14 @@ public class Practiec {
 
 	public static void main(String[] args) {
 
+		
+		
 		for (int i = 10; i >= 1; i--) {
 			for (int x = 1; x <= i; x++) {
 				System.out.print("*");
 
 			}
 			System.out.print("\n");
-
 		}
 
 	}
