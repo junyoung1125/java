b4df63da6b105ce200fe915a54b6fc54b0a90bfb
wnsdud060451 <wnsdud060451@gmail.com>
2025-01-03 17:45:51 +0900

    add ThisJava

---
+++ ThisJava/src/ch04/sec05/Preactice.java
@@ -0,0 +1,15 @@
+package ch04.sec05;
+
+public class Preactice {
+
+	public static void main(String[] args) {
+
+		for (int i = 2; i <= 9; i++) {
+			for (int y = 1; y <= 9; y++) {
+				int z = i * y;
+				System.out.println(i + "x" + y + "=" + z);
+			}
+		}
+	}
+
+}
