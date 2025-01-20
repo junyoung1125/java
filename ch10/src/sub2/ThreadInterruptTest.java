ed5651a2d36b9ed60239069081ead03aa9e06444
junyoung1125 <wnsdud060451@gmail.com>
2025-01-20 12:46:19 +0900

    ch10

---
+++ ch10/src/sub2/ThreadInterruptTest.java
@@ -0,0 +1,50 @@
+package sub2;
+/*
+ * 날짜: 2025/02/10
+ * 이름: 변준영
+ * 내용: Java 스레드 Interrupt 실습하기
+ */
+
+class SubThread extends Thread {
+	@Override
+	public void run() {
+
+		int i = 1;
+		try {
+			while (true) {
+				System.out.println("SubThread 실행..." + i);
+				i++;
+
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+		}
+		System.out.println("SubThread 종료...");
+	}
+}
+
+public class ThreadInterruptTest {
+
+	public static void main(String[] args) {
+
+		SubThread sub = new SubThread();
+
+		// 스레드 실행
+		sub.start();
+
+		try {
+			// 10초 대기
+			Thread.sleep(1000 * 10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// 서브 스레드 일시 정지 상태에서 종료상태로 전환
+		sub.interrupt();
+
+		System.out.println("메인 스레드 종료...");
+	}
+
+}
